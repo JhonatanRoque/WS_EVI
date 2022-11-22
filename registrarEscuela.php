c152eb7d4c3d1fdb472236d213d81b4d1ef6f63a
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-22 16:19:56 -0600

    cambios, ver datos que se recibem

--- registrarEscuela.php
+++ registrarEscuela.php
@@ -10,16 +10,12 @@ $contrasena = htmlspecialchars($_POST['contrasena'], ENT_QUOTES);
 $codigoSeguridad = htmlspecialchars($_POST['codigoS'], ENT_QUOTES);
 
 if(($nombre != "")){
-    $resultado = escuela::regEscuela($nombre, $direccion, $telefono, $ $correo, $contrasena, $codigoSeguridad); //Realizamos la ejecución de nuestra funcion para registrar escuela, pasandole los datos que recibimos anteriormente
-    if ($resultado==1) { //Comprobamos que recibimos un 1 como señal de que se registro la escuela
-        header('Content-type: application/json; charset=utf-8'); //Encabezado para indicar al navegador, que debe mostrar la pagina en formato application/json
-        $json_string = json_encode(array("estado" => 1,"mensaje" => "Escuela registrada correctamente.")); //Preparamos el mensaje a enviar como respuesta
-        echo $json_string; //Imprimimos el mensaje en pantalla
-    } else {
-        header('Content-type: application/json; charset=utf-8');
-        $json_string = json_encode(array("estado" => 2,"mensaje" => $resultado));
-		echo $json_string;
-    }
+   echo $nombre."<br>";
+   echo $contrasena."<br>";
+   echo $direccion."<br>";
+   echo $telefono."<br>";
+   echo $correo."<br>";
+   echo $codigoSeguridad."<br>";
 }else {
     header('Content-type: application/json; charset=utf-8');
     $json_string = json_encode(array("estado" => 0,"mensaje" => "Faltan datos para procesar la solicitud $nombre"));
