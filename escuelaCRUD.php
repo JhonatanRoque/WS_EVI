9fc30801e253e26b78d39865538f58f9040b85a2
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-22 21:22:39 -0600

    iniciar Sesion

--- escuelaCRUD.php
+++ escuelaCRUD.php
@@ -33,6 +33,29 @@ class escuela{
 
     }
 
+    //Método para iniciar sesión
+    public static function getLogin($correo, $contrasena){
+        include("connection.php");
+    
+    // Consulta de la tabla empresa para verificar si existe una empresa registrada con dichos datos.
+    $query = "SELECT * FROM tbEscuelas WHERE  correo = ? and contrasena = ?"; //Sentencia SQL para consultar datos en la tabla escuelas
+    try {    
+          $link=conexion();    
+          $comando = $link->prepare($query);
+          $comando->execute(array($correo,$contrasena));
+          $row = $comando->fetch(PDO::FETCH_ASSOC);
+          $filasAfectadas = $comando->rowCount();
+          if( $filasAfectadas > 0){
+            return $row;
+          }
+          $mensaje = array("mensaje" =>"Correo o contraseña incorrectos, puede que no exista una escuela con dichas credenciales");
+          return $mensaje;
+
+        } catch (PDOException $e) {
+            return -1;
+        }
+    }
+
     //Metodo para comprobar si ya existe un cuenta con dicho nombre
     public static function checkName($nombre){
         $query = "SELECT nombre FROM tbEscuelas WHERE nombre = ?";
---
+++ iniciarSession.php
@@ -0,0 +1,21 @@
+<?php
+    include("escuelaCRUD.php");
+    $correo = htmlspecialchars($_POST['correo'], ENT_QUOTES);
+    $contrasena = htmlspecialchars($_POST['contrasena'], ENT_QUOTES);
+
+    if(($correo != "") and ($contrasena != "")){
+    
+        $resultado = escuela::getLogin($correo, $contrasena);
+            
+        header('Content-type: application/json; charset=utf-8');
+        $json_string = json_encode($resultado);
+        echo $json_string;
+        
+            
+    } else {
+        header('Content-type: application/json; charset=utf-8');
+        $json_string = json_encode(array("estado" => 0, "mensaje" => "Hay datos que no se han suministrado $empresa + $contrasena"));
+        echo $json_string;
+    }
+
+?>
