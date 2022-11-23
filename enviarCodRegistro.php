ba68bbbedf72e4a6fed48b424427cc7303873f2f
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-22 19:34:22 -0600

    cod

--- enviarCodRegistro.php
+++ enviarCodRegistro.php
@@ -2,7 +2,6 @@
 include("escuelaCRUD.php");
 
 $destinatario = "franciscoabarca7392@gmail.com";
-$from = "franciscoabarca@transportfast.xyz";
 $asunto = "Codigo para registrar empresa";
 $codigo = escuela::getCodigoV();
 $CC = "familiamenendezreyes@gmail.com";
@@ -21,14 +20,9 @@ $cuerpo = '
 '; 
 
 //para el envío en formato HTML 
-$cabeceras  = 'MIME-Version: 1.0' . "\r\n";
-$cabeceras .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-// Cabeceras adicionales
-$cabeceras .= 'To: ' . $destinatario . "\r\n";
-$cabeceras .= 'From: '. $from . "\r\n";
-$cabeceras .= 'Cc: ' . $CC .  "\r\n";
-if (!mail($destinatario, $asunto, $cuerpo, $cabeceras)){
+$headers = 'MIME-Version: 1.0' . "\r\n"; 
+$headers = 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+if (!mail($destinatario, $asunto, $cuerpo, $headers)){
     header('Content-type: application/json; charset=utf-8');
     $json_string = json_encode(array("mensaje" => "Ocurrio un error al enviar el email"));
     echo $json_string;
