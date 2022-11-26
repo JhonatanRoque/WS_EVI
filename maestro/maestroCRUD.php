6b1cd97d482a750d4425d19def6fc969dab03e1f
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-26 10:01:30 -0600

    connection.php agregado

--- maestro/maestroCRUD.php
+++ maestro/maestroCRUD.php
@@ -4,6 +4,7 @@
 class maestro{
     //Método para iniciar sesión
     public static function getLogin($correo, $contrasena){
+    include("connection.php");
         // Consulta de la tabla maestros para verificar si existe un maestro registrado con dichos datos.
     $query = "SELECT * FROM tbMaestros WHERE  correo = ? and contrasena = ?"; //Sentencia SQL para consultar datos en la tabla
     try {    
