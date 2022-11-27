96f6e749b008920a67a90104a5e6f144a7ff5eea
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-26 20:02:32 -0600

    cambios

--- getMaterias.php
+++ getMaterias.php
@@ -2,13 +2,9 @@
 include("escuelaCRUD.php");
 
     $resultado == escuela::getMateria();
-    if($resultado > 0){
+ 
         echo $resultado;
-    }else {
-        header('Content-type: application/json; charset=utf-8');
-        $json_string = json_encode(array("estado" => 2,"mensaje" => "No se encontraron grados relacionados a su institución, si es administrador agregue los grados respectivos."));
-        echo $json_string;
-    }
+   
 
 
 ?>
