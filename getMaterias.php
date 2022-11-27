310eaba3cd928ab14b3ee045010910d942bc8f18
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-27 17:14:13 -0600

    cambios

--- getMaterias.php
+++ getMaterias.php
@@ -2,8 +2,13 @@
 include("escuelaCRUD.php");
 
     $resultado == escuela::getMateria();
- 
+    if($resultado){
         echo $resultado;
+    }else{
+        header('Content-type: application/json; charset=utf-8');
+        $json_string = json_encode(array("estado" => 0,"mensaje" => "No se encontraron datos que mostrar"));
+        echo $json_string;
+    }
    
 
 
