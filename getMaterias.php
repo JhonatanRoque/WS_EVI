b95d6fdaa2ba267dd46f18958b8b45b9c0c2491f
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-26 20:01:15 -0600

    cambios

--- getMaterias.php
+++ getMaterias.php
@@ -1,9 +1,6 @@
 <?php
 include("escuelaCRUD.php");
 
-$escuelaID = htmlspecialchars($_POST['escuelaID'], ENT_QUOTES);
-
-if($escuelaID != ""){
     $resultado == escuela::getMateria();
     if($resultado > 0){
         echo $resultado;
@@ -12,10 +9,6 @@ if($escuelaID != ""){
         $json_string = json_encode(array("estado" => 2,"mensaje" => "No se encontraron grados relacionados a su institución, si es administrador agregue los grados respectivos."));
         echo $json_string;
     }
-}else {
-    header('Content-type: application/json; charset=utf-8');
-    $json_string = json_encode(array("estado" => 0,"mensaje" => "Faltan datos para procesar la solicitud ".$escuelaID));
-    echo $json_string;
-}
+
 
 ?>
