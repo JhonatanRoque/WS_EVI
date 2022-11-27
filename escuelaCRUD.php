82a7cf51aae29df85a0253e04ac24bfe6f2a98cc
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-27 17:28:51 -0600

    priba de error

--- escuelaCRUD.php
+++ escuelaCRUD.php
@@ -311,26 +311,12 @@ class escuela{
 
     //Método para pbtener las matetrias
     public static function getMateria() {
-        $conn = null;
-	$host = "localhost";
-	$db = "u701462728_db_evi"; //Aqui ira el nombre de nuestra DB
-	$user = "u701462728_admin"; //Aqui ira el nombre de nuestro usuario de BD
-	$pwd = "Jona1.than6"; //Contraseña de BD
-
-	try{
-		$conn = new PDO('mysql:host='.$host.'; dbname='.$db, $user,$pwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
-		//echo "connexion hecha";
-	}catch(PDOException $e){
-		echo "<center><h2 style='color:green'>Error al tratar de conectar a la DB. ";
-		echo " consulte al soporte Técnico </h2></center>";
-
-		exit();
-	}
+        include("connection.php");
         
-        $query = "SELECT * FROM tbMateria;";
+        $query = "SELECT * FROM tbMateriaswfrwsvdf;";
 
         try {
-            $link= $conn;    
+            $link=conexion();    
             $comando = $link->prepare($query);
             // Ejecutar sentencia preparada
             $comando->execute();
