96a53f6cdfa88de18eff11fe6cb74225b417c516
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-11-29 17:15:42 -0600

    obtener grados

--- gradosActivos.php
+++ gradosActivos.php
@@ -6,6 +6,9 @@ $escuelaID = $_POST['escuelaID'];
     $resultado = escuela::getGradosInt($escuelaID);
     if($resultado){
         echo $resultado;
+        header('Content-type: application/json; charset=utf-8');
+        $json_string = json_encode(array("grados" => $resultado));
+        echo $json_string;
     }else{
         header('Content-type: application/json; charset=utf-8');
         $json_string = json_encode(array("estado" => 0,"mensaje" => "No se encontraron datos que mostrar"));
