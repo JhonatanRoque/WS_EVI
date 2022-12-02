7fa141f69cc53a1c6fb97308f8809efccf825952
JhonatanRoque <franciscoabarca7392@gmail.com>
2022-12-02 10:51:40 -0600

    asignarmaesatro con materias

---
+++ setMateriaMaeestro.php
@@ -0,0 +1,25 @@
+<?php
+//Incluimos nuestro archivo crud
+include("escuelaCRUD.php");
+//Leemos todos los datos que nos envia la APP
+$materiaID = htmlspecialchars($_POST['materiaID'], ENT_QUOTES);
+$maestroID = htmlspecialchars($_POST['maestroID'], ENT_QUOTES);
+
+if(($maestroID != "") && ($materiaID != "")){
+    $resultado = escuela::setMateriaMaestro($maestroID, $materiaID); //Realizamos la ejecución de nuestra funcion para registrar la materia, pasandole los datos que recibimos anteriormente
+    if ($resultado==1) { //Comprobamos que recibimos un 1 como señal de que se registro el grado
+        header('Content-type: application/json; charset=utf-8'); //Encabezado para indicar al navegador, que debe mostrar la pagina en formato application/json
+        $json_string = json_encode(array("estado" => 1,"mensaje" => "Materia registrada correctamente.")); //Preparamos el mensaje a enviar como respuesta
+        echo $json_string; //Imprimimos el mensaje en pantalla
+    } else {
+        header('Content-type: application/json; charset=utf-8');
+        $json_string = json_encode(array("estado" => 2,"mensaje" => "No se pudo registrar la materia $nombre, verifique los datos"));
+		echo $json_string;
+    }
+}else {
+    header('Content-type: application/json; charset=utf-8');
+    $json_string = json_encode(array("estado" => 0,"mensaje" => "Faltan datos para procesar la solicitud $nombre"));
+    echo $json_string;
+}
+
+?>
